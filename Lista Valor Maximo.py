9f7735914a573a67a53e7260f702f78459a41891
Junior Tonoli <tailsapnea@gmail.com>
2020-06-22 21:13:53 -0300

    Add files via upload

--- Lista Valor Maximo.py
+++ Lista Valor Maximo.py
@@ -23,3 +23,38 @@ while True:
             else:
                 print(f'{c} ', end='')
         print()
+
+#selecao em lista
+vetor = []
+for c in range(0, 11):
+    x = int(input(f'valor X:'))
+    vetor.append(x)
+for k, v in enumerate(vetor):
+    if v <=10:
+        print(f'{k} = {v}')
+# selecao em lista alternate
+# A=[]
+# for i in range(100):
+#     x = float(input())
+#     A.append(x)
+#     if x <=10.0:
+#         print('A[{}] = {}'.format(i,x))
+
+# criando uma lista com base na outra, reverse [start:stop:step]
+N=[]
+for i in range(20):
+    x=int(input())
+    N.append(x)
+a=N[::-1]
+for j in range(20):
+    print('N[{}] = {}'.format(j,a[j]))
+
+# [min maximo lista e index]
+
+n = int(input(f'valor n:'))
+lista = []
+for c in range(n):
+    x = int(input(f'valor X{c}:'))
+    lista.append(x)
+print(f' {min(lista)}')
+print(f' {lista.index(min(lista))}')
